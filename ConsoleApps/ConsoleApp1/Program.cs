55bc8482cde1190b5550b943ddbf4505998aca1d
TopSwagCode <josh@topswagcode.com>
2025-03-05 10:44:28 +0100

    init

---
+++ ConsoleApps/ConsoleApp1/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+File.WriteAllText("/app/shared/output.txt", "Hello from ConsoleApp1!");
---
+++ ConsoleApps/ConsoleApp2/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string content = File.ReadAllText("/app/shared/test.txt");
+Console.WriteLine($"Read from shared volume: {content}. From ConsoleApp2!");
